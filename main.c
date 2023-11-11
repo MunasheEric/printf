59cdc9d5919669b9b9c83c25ff16e299e21fda87
MunasheEric <munasheeric@gmail.com>
2023-11-11 10:36:46 -0800

    main.c

--- main.c
+++ main.c
@@ -19,38 +19,34 @@ int main(void)
 
 {
 
-	    int len;
+	int len;
+	int len2;
 
-	        int len2;
+	unsigned int ui;
 
-		    unsigned int ui;
+	void *addr;
 
-		        void *addr;
+	len = _printf("Let's try to printf a simple sentence.\n");
 
+	len2 = printf("Let's try to printf a simple sentence.\n");
 
-			    len = _printf("Let's try to printf a simple sentence.\n");
+	ui = (unsigned int)INT_MAX + 1024;
 
-			        len2 = printf("Let's try to printf a simple sentence.\n");
+	addr = (void *)0x7ffe637541f0;
+	_printf("Length:[%d, %i]\n", len, len);
 
-				    ui = (unsigned int)INT_MAX + 1024;
+	printf("Length:[%d, %i]\n", len2, len2);
 
-				        addr = (void *)0x7ffe637541f0;
+	_printf("Negative:[%d]\n", -762534);
 
-					    _printf("Length:[%d, %i]\n", len, len);
+	printf("Negative:[%d]\n", -762534);
 
-					        printf("Length:[%d, %i]\n", len2, len2);
+	_printf("Unsigned:[%u]\n", ui);
 
-						    _printf("Negative:[%d]\n", -762534);
+	printf("Unsigned:[%u]\n", ui);
 
-						        printf("Negative:[%d]\n", -762534);
-
-							    _printf("Unsigned:[%u]\n", ui);
-
-							        printf("Unsigned:[%u]\n", ui);
-
-								    _printf("Unsigned octal:[%o]\n", ui);
-
-								        printf("Unsigned octal:[%o]\n", ui);
+	_printf("Unsigned octal:[%o]\n", ui);
+	printf("Unsigned octal:[%o]\n", ui);
 
 									    _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
 
@@ -80,6 +76,6 @@ int main(void)
 
 															        printf("Unknown:[%r]\n");
 
-																    return (0);
+																  return (0);
 
 }
