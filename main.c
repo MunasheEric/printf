32a5f8342944c2281878a8f80abc6e7b735fa04a
MunasheEric <munasheeric@gmail.com>
2023-11-11 10:50:09 -0800

    main.c

--- main.c
+++ main.c
@@ -6,14 +6,14 @@
 
 
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0
- *
- *     */
+*
+** main - Entry point
+*
+**
+*
+** Return: Always 0
+*
+**/
 
 int main(void)
 
@@ -48,34 +48,33 @@ int main(void)
 	_printf("Unsigned octal:[%o]\n", ui);
 	printf("Unsigned octal:[%o]\n", ui);
 
-									    _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+			_printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
 
-									        printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+			printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
 
-										    _printf("Character:[%c]\n", 'H');
+					_printf("Character:[%c]\n", 'H');
 
-										        printf("Character:[%c]\n", 'H');
+					printf("Character:[%c]\n", 'H');
 
-											    _printf("String:[%s]\n", "I am a string !");
+					_printf("String:[%s]\n", "I am a string !");
 
-											        printf("String:[%s]\n", "I am a string !");
+	printf("String:[%s]\n", "I am a string !");
 
-												    _printf("Address:[%p]\n", addr);
+	_printf("Address:[%p]\n", addr);
 
-												        printf("Address:[%p]\n", addr);
+	printf("Address:[%p]\n", addr);
 
-													    len = _printf("Percent:[%%]\n");
+	len = _printf("Percent:[%%]\n");
 
-													        len2 = printf("Percent:[%%]\n");
+	len2 = printf("Percent:[%%]\n");
 
-														    _printf("Len:[%d]\n", len);
+	_printf("Len:[%d]\n", len);
 
-														        printf("Len:[%d]\n", len2);
+printf("Len:[%d]\n", len2);
 
-															    _printf("Unknown:[%r]\n");
+	_printf("Unknown:[%r]\n");
 
-															        printf("Unknown:[%r]\n");
+	printf("Unknown:[%r]\n");
 
-																  return (0);
-
+return (0);
 }
